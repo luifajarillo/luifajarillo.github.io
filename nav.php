697cac9eaed41edcd9c2553a14406f7767abc8d0
luifajarillo <85654498+luifajarillo@users.noreply.github.com>
2024-12-11 01:30:21 +0800

    update

---
+++ nav.php
@@ -0,0 +1,6 @@
+<div id="nav">
+	<a class="pages" href="oops.png" title="View Users">About Me</a>
+	<a class="pages" href="oops.png" title="Registration Page">Education & Skills</a>
+	<a class="pages" href="oops.png" title="View Users">Projects</a>
+	<a class="pages" href="oops.png" title="View Users">Contact</a>
+</div>
