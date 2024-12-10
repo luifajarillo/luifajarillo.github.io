09317d3afff3898b569d9e500a1c479dc0fd68ce
luifajarillo <85654498+luifajarillo@users.noreply.github.com>
2024-12-11 01:33:46 +0800

    update

--- header.php
+++ header.php
@@ -1,3 +1,10 @@
 <div id="header">
 	<h1>J LOUISSE</h1>
 </div>
+
+<div id="nav">
+	<a class="pages" href="oops.png" title="View Users">About Me</a>
+	<a class="pages" href="oops.png" title="Registration Page">Education & Skills</a>
+	<a class="pages" href="oops.png" title="View Users">Projects</a>
+	<a class="pages" href="oops.png" title="View Users">Contact</a>
+</div>
